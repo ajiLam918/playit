14f91a670d602a4e5b2beb4db6f8d54a08ea6c80
ajiLam918 <167869006+ajiLam918@users.noreply.github.com>
2024-07-28 06:44:01 +0700

    Update Install-Playit.ps1

--- Install-Playit.ps1
+++ Install-Playit.ps1
@@ -1,17 +1,17 @@
-# Mengunduh playit.gg
-Invoke-WebRequest -Uri "https://playit.gg/downloads/playit-windows.zip" -OutFile "$env:USERPROFILE\playit-windows.zip"
+# Tentukan URL untuk mengunduh playit.gg MSI
+$msiUrl = "https://github.com/playit-cloud/playit-agent/releases/download/v0.15.13/playit-windows-x86.msi"
 
-# Membuat direktori tujuan jika belum ada
-if (-Not (Test-Path -Path "$env:USERPROFILE\playit")) {
-    New-Item -ItemType Directory -Path "$env:USERPROFILE\playit09"
-}
+# Tentukan path file MSI sementara
+$msiFilePath = "$env:USERPROFILE\Downloads\playit-windows.msi"
 
-# Ekstrak file zip ke direktori tujuan
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::ExtractToDirectory("$env:USERPROFILE\playit-windows.zip", "$env:USERPROFILE\playit09")
+# Unduh file MSI
+Invoke-WebRequest -Uri $msiUrl -OutFile $msiFilePath
 
-# Hapus file zip setelah ekstraksi
-Remove-Item -Path "$env:USERPROFILE\playit-windows.zip"
+# Instal MSI
+Start-Process msiexec.exe -ArgumentList "/i", $msiFilePath, "/quiet", "/norestart" -NoNewWindow -Wait
 
-# Jalankan playit.gg
-Start-Process -FilePath "$env:USERPROFILE\playit\playit09\playit.exe"
+# Hapus file MSI setelah instalasi
+Remove-Item -Path $msiFilePath
+
+# Menjalankan playit.gg jika diperlukan (ubah path sesuai dengan lokasi instalasi playit.gg)
+Start-Process -FilePath "C:\Program Files\playit\playit.exe"
